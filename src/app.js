7030f21b9dabd8d9f83f5e7c484363ffe1fe3ccf
fuad sanin <fuhadsanin28@gmail.com>
2021-10-08 15:36:16 -0700

    Outer Home Center part completed

--- src/app.js
+++ src/app.js
@@ -1,21 +1,6 @@
 import React from "react";
-import jumboData from '../fixtures/jumbo'
-import Jumbotron from "./components/jumbotron";
-
-
+import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-            <p>hello</p>
-        </Jumbotron>
-      ))}
-
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
-
-
